6ce6df4edfd098379fe060f1032598806db8aa0a
Sonic <sonicshadow6@gmail.com>
2015-11-21 16:30:51 -0800

    important functional changes

--- app/src/main/java/com/example/android/batterytimerapp/MainActivity.java
+++ app/src/main/java/com/example/android/batterytimerapp/MainActivity.java
@@ -5,19 +5,46 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
     private TextView contentTxt;
+    Activity loseText = this;
     private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver(){
         @Override
         public void onReceive(Context arg0, Intent intent) {
             // TODO Auto-generated method stub
             int level = intent.getIntExtra("level", 0);
             contentTxt.setText(String.valueOf(level) + "%");
-        }
+
+
+
+            if (level<35) {
+
+
+
+                TimerTask endtimer = new TimerTask() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(loseText, "You Lost!", Toast.LENGTH_LONG);
+                    }
+                };
+
+                Timer timerObject = new Timer();
+                timerObject.schedule(endtimer, 2000);
+
+
+
+                }
+
+            };
     };
 
     @Override
@@ -28,7 +55,14 @@ public class MainActivity extends Activity {
         this.registerReceiver(this.mBatInfoReceiver,
                 new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
+    //public boolean isCharging(){
+
+//}
+
+
 }
+
+
 
 
 //hi people
